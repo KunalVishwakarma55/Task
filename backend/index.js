0749ac42160dc237c9e8f3a91dacdedecf9e8d95
Kunal Vishwakarma <kunalvishwakarma7185@gmail.com>
2026-03-04 23:39:49 +0530

    Update CORS origin for production deployment

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["http://localhost:5173" , "http://localhost:5174"],
+ origin:["https://task-frontend-smj2.onrender.com" , "http://localhost:5174"],
  credentials:true
 }))
 
